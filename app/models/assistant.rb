b17d632d36a5af4f42d97c29577863eedbd16edd
brandoshmando <brancraft@gmail.com>
2014-07-22 20:53:41 -0400

    User edit method is working. Added class method to both Professor and Assistant models so that they can use the parent class's path helper

--- app/models/assistant.rb
+++ app/models/assistant.rb
@@ -1,3 +1,7 @@
 class Assistant < User
   belongs_to :course
+  #Redefines model name method to return User.model_name instead. This lets each class use its parents path helpers
+  def self.model_name
+    User.model_name
+  end
 end
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,4 +1,9 @@
 class Professor < User
   has_many :courses
   has_many :evals
+
+  #Redefines model name method to return User.model_name instead. This lets each class use its parents path helpers
+  def self.model_name
+    User.model_name
+  end
 end
