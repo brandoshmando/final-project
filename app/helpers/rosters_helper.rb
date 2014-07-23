ea11dd406e4e72c06d327d3c1f641aa29ba0866f
brandoshmando <brancraft@gmail.com>
2014-07-23 16:50:17 -0400

    Added helper method to rosters view to populate time select. Added time select...

--- app/helpers/rosters_helper.rb
+++ app/helpers/rosters_helper.rb
@@ -2,4 +2,8 @@ module RostersHelper
   def days_of_week
     return [["Monday", "Monday"],["Tuesday", "Tuesday"], ["Wednesday", "Wednesday"], ["Thursday", "Tuesday"], ["Friday", "Friday"]]
   end
+
+  def time_slots
+   return (1..24).to_a
+  end
 end
