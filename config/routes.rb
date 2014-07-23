f2dfaa1977dabfe2221177946ce82825ed4928c8
Alessia Bellisario <bellisario.alessia@gmail.com>
2014-07-23 15:05:58 -0400

    Added routes for Evals Categories and Comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   resources :courses, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update, :destroy]
 
+  resources :evals, only: [:new, :create, :edit, :update, :destroy], shallow: true do
+    resources :categories, only: [:new, :create, :edit, :update, :destroy], shallow: true do
+      resources :comments, only: [:new, :create, :edit, :update, :destroy]
+    end
+  end
+
+resources
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
