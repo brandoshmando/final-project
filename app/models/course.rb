85f128a6cb3d5f289bd464bc254db95751a8e6b5
brandoshmando <brancraft@gmail.com>
2014-07-22 22:09:51 -0400

    Set up the has and belongs to many relationships between assistant and course so that it actually works. Added option to point to a join table with a differnet name

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
-  has_many :assistants
   has_many :rosters
   has_many :students, :through => :rosters
   belongs_to :professor
+  has_and_belongs_to_many :assistants, join_table: 'assistants_courses'
   has_and_belongs_to_many :evals
 end
