31c75d921904187b78e1a66a8bd87180595d526e
Alessia Bellisario <bellisario.alessia@gmail.com>
2014-07-27 13:38:58 -0400

    Added cancancan gem to gemfile

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'haml-rails'
 gem 'bcrypt', '~> 3.1.7'
 gem 'cocoon'
 gem 'sorcery'
+gem 'cancancan', '~> 1.9'
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 group :development do
   gem 'pry'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   validates :password, :presence => true, :confirmation => true, :on => :update
   validates :first_name, :presence => true, :on => :update
   validates :last_name,  :presence => true, :on => :update
+  validates_uniqueness_of :email
   #Allows user to be created without until activated password using sorcery gem
   before_create :setup_activation
   #Sorcery method to set external auth (Oauth) to false
