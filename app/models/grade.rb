ab230977b0735bcd0321604571cdd9a8709fdd0e
brandoshmando <brancraft@gmail.com>
2014-07-27 12:55:16 -0400

    Setup grades model and new relationships

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,5 +1,6 @@
 class Grade < ActiveRecord::Base
   belongs_to :student
-  belongs_to :category
+  has_many :scores
   has_and_belongs_to_many :comments
+  accepts_nested_attributes_for :scores
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726205830) do
+ActiveRecord::Schema.define(version: 20140727164259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20140726205830) do
   end
 
   create_table "grades", force: true do |t|
-    t.decimal  "score"
+    t.decimal  "final_score"
     t.integer  "category_id"
     t.integer  "student_id"
     t.integer  "eval_id"
@@ -77,6 +77,13 @@ ActiveRecord::Schema.define(version: 20140726205830) do
     t.string   "section_name"
     t.string   "location"
     t.integer  "course_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "scores", force: true do |t|
+    t.decimal  "score"
+    t.integer  "grade_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
