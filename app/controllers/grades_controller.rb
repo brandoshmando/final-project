5d29b6371191b320cc5f320301166f1ba470d7fc
brandoshmando <brancraft@gmail.com>
2014-07-27 18:59:54 -0400

    Added funcitonality in grades controller to build dynamic number of scores based on the number of categories in the given eval. Form then loobs through each one and renders separate fields for each.

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,17 +1,17 @@
 class GradesController < ApplicationController
-  before_action :load_eval
+  before_action :load_eval, only:[:new]
   before_action :load_student
   def new
     @grade = Grade.new
-    @score = @grade. scores.build
+    (@eval.categories.count).times { @grade.scores.build}
   end
 
   def create
-    @grade = Grade.build(grades_params)
+    @grade = Grade.new(grades_params)
     @grade.student_id = @student.id
 
-    if @student.save
-      redirect_to roster_path(@student.roster), notice: "#{@eval.title} successfully graded for #{@student.first_name} #{@student.last_name}"
+    if @grade.save
+      redirect_to roster_path(@student.roster), notice: "#{@grade.title} successfully graded for #{@student.first_name} #{@student.last_name}!"
     else
       render :new
     end
@@ -21,6 +21,7 @@ class GradesController < ApplicationController
   def grades_params
     params.require(:grade).permit(:title, scores_attributes: [:id, :score, :_delete])
   end
+
   def load_eval
     @eval = Eval.find(params[:eval_id])
   end
