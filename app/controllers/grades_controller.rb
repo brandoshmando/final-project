1459d1d7f2efa5c1f9efddb15af39e44ed2ec904
brandoshmando <brancraft@gmail.com>
2014-07-27 14:48:46 -0400

    Nested form for grades with hidden field that fills value of grade title

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,13 +1,14 @@
 class GradesController < ApplicationController
   before_action :load_eval
+  before_action :load_student
   def new
-    @student = Student.find(params[:student_id])
-    @grade = @student.grades.build
-    @score = @grade.scores.build
+    @grade = Grade.new
+    @score = @grade. scores.build
   end
 
   def create
-    @student.grades.build(grades_params)
+    @grade = Grade.build(grades_params)
+    @grade.student_id = @student.id
 
     if @student.save
       redirect_to roster_path(@student.roster), notice: "#{@eval.title} successfully graded for #{@student.first_name} #{@student.last_name}"
@@ -18,9 +19,13 @@ class GradesController < ApplicationController
 
   private
   def grades_params
-    params.require(:grade).permit(:title, scores_attributes: [:score])
+    params.require(:grade).permit(:title, scores_attributes: [:id, :score, :_delete])
   end
   def load_eval
     @eval = Eval.find(params[:eval_id])
+  end
+
+  def load_student
+    @student = Student.find(params[:student_id])
   end
 end
