03ca8aa1a2fdcd2eceb0c8dfe8683e1627dada94
brandoshmando <brancraft@gmail.com>
2014-07-31 00:26:12 -0400

    One tweak to link_or_grade method. Also added total score column

--- app/helpers/rosters_helper.rb
+++ app/helpers/rosters_helper.rb
@@ -9,7 +9,7 @@ module RostersHelper
 
   def link_or_grade(student, eval_id)
     container = student.grades.where("eval_id = ?", eval_id)
-    if container[0] && container[0].final_score
+    if container[0]
       format_score(container[0].final_score)
     else
       link_to "Grade", new_student_grade_path(student, eval_id: eval_id)
