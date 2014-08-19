6d3c90d5ca7144b7edc0838ccb81afab441def5d
brandoshmando <brancraft@gmail.com>
2014-08-19 16:33:52 -0400

    First chart loads on click but second does not for some reason. Also, first chart does not load on page load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,76 @@
 //= require_tree .
 
 $(document).on('page:load ready', function(){
+  var courses = $('.course-chart');
+  var course;
+  function generateChart(index) {
+
+    // AmCharts.ready(function(){
+
+      course = courses[index]
+      var studentPoints = JSON.parse(course.dataset.studentPoints)
+      console.log(studentPoints)
+      var chart = new AmCharts.AmSerialChart();
+      chart.autoMargins = true
+      chart.dataProvider = []
+      chart.autoMarginOffset = 0;
+      chart.theme = "none"
+
+
+      //Define X Axis (Category Field)
+      chart.categoryField = 'x'
+
+      //Define Category Axis attributes
+      chart.categoryAxis.minorGridEnabled = true;
+      chart.categoryAxis.minorGridAlpha = 0.1;
+      chart.categoryAxis.axisColor = '#28384B'
+
+      // Graph
+      var graph = new AmCharts.AmGraph();
+      graph.valueField = "y";
+      graph.type = "column";
+      graph.fillAlphas = 0.8;
+      graph.lineAlpha = 0.2;
+      graph.balloonColor = '#2ED959'
+      graph.fillColors = '#2ED959'
+
+
+      chart.addGraph(graph)
+
+      // Call function to populate chart
+      generateData(studentPoints, chart);
+
+      //Chart Styling
+      chart.gridAboveGraphs = true;
+      chart.startDuration = 1;
+      chart.categoryAxis.gridAlpha = 0;
+      chart.categoryAxis.title = "Percentile";
+
+      //Add Chart cursor
+      var chartCursor = new AmCharts.ChartCursor();
+      chartCursor.zoomable = false;
+      chartCursor.alpha = 0.9;
+      chartCursor.cursorColor = '#28384B'
+      chartCursor.categoryBalloonEnabled = false;
+      chart.addChartCursor(chartCursor);
+
+      //Write
+      chart.write("chart" + index)
+
+
+
+      function generateData(studentPoints){
+        console.log("called")
+        var dataProvider = [];
+        for (var i=0; i < studentPoints.length; i++){
+        dataProvider.push({x:((i + 1) * 10) + "th", y:studentPoints[i] })
+        }
+        console.log(dataProvider)
+        chart.dataProvider = dataProvider;
+        chart.validateNow();
+      };
+    // });
+  };
   // All logic for grade form starts here
   $('input[type=checkbox]').hide();
 
@@ -44,8 +114,8 @@ $(document).on('page:load ready', function(){
 
   $(".js-vertical-tab").click(function(event) {
     event.preventDefault();
-    var index = JSON.parse($(this).data('index'))
-    console.log(index)
+    var index = $(this).data('index')
+    generateChart(index);
 
     $(".js-vertical-tab-content").hide();
     var activeTab = $(this).attr("rel");
--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,72 +1,72 @@
-$(document).on('page:laod ready', function(){
-  //Course curve chart
+// $(document).on('page:laod ready', function(){
+//   //Course curve chart
 
-  var courses = $('.course-chart');
-  var course;
-  function generateChart(index) {
-    AmCharts.ready(function(){
-      course = courses[index]
-      var studentPoints = JSON.parse(course.dataset.studentPoints)
-      console.log(studentPoints)
-      var chart = new AmCharts.AmSerialChart();
-      chart.autoMargins = true
-      chart.dataProvider = []
-      chart.autoMarginOffset = 0;
-      chart.theme = "none"
-
-
-      //Define X Axis (Category Field)
-      chart.categoryField = 'x'
-
-      //Define Category Axis attributes
-      chart.categoryAxis.minorGridEnabled = true;
-      chart.categoryAxis.minorGridAlpha = 0.1;
-      chart.categoryAxis.axisColor = '#28384B'
-
-      // Graph
-      var graph = new AmCharts.AmGraph();
-      graph.valueField = "y";
-      graph.type = "column";
-      graph.fillAlphas = 0.8;
-      graph.lineAlpha = 0.2;
-      graph.balloonColor = '#2ED959'
-      graph.fillColors = '#2ED959'
+//   var courses = $('.course-chart');
+//   var course;
+//   function generateChart(index) {
+//     AmCharts.ready(function(){
+//       course = courses[index]
+//       var studentPoints = JSON.parse(course.dataset.studentPoints)
+//       console.log(studentPoints)
+//       var chart = new AmCharts.AmSerialChart();
+//       chart.autoMargins = true
+//       chart.dataProvider = []
+//       chart.autoMarginOffset = 0;
+//       chart.theme = "none"
 
 
-      chart.addGraph(graph)
+//       //Define X Axis (Category Field)
+//       chart.categoryField = 'x'
 
-      // Call function to populate chart
-      generateData(studentPoints, chart);
+//       //Define Category Axis attributes
+//       chart.categoryAxis.minorGridEnabled = true;
+//       chart.categoryAxis.minorGridAlpha = 0.1;
+//       chart.categoryAxis.axisColor = '#28384B'
 
-      //Chart Styling
-      chart.gridAboveGraphs = true;
-      chart.startDuration = 1;
-      chart.categoryAxis.gridAlpha = 0;
-      chart.categoryAxis.title = "Percentile";
-
-      //Add Chart cursor
-      var chartCursor = new AmCharts.ChartCursor();
-      chartCursor.zoomable = false;
-      chartCursor.alpha = 0.9;
-      chartCursor.cursorColor = '#28384B'
-      chartCursor.categoryBalloonEnabled = false;
-      chart.addChartCursor(chartCursor);
-
-      //Write
-      chart.write("chart" + i)
+//       // Graph
+//       var graph = new AmCharts.AmGraph();
+//       graph.valueField = "y";
+//       graph.type = "column";
+//       graph.fillAlphas = 0.8;
+//       graph.lineAlpha = 0.2;
+//       graph.balloonColor = '#2ED959'
+//       graph.fillColors = '#2ED959'
 
 
+//       chart.addGraph(graph)
 
-      function generateData(studentPoints){
-        console.log("called")
-        var dataProvider = [];
-        for (var i=0; i < studentPoints.length; i++){
-        dataProvider.push({x:((i + 1) * 10) + "th", y:studentPoints[i] })
-        }
-        console.log(dataProvider)
-        chart.dataProvider = dataProvider;
-        chart.validateNow();
-      };
-    });
-  };
-})
+//       // Call function to populate chart
+//       generateData(studentPoints, chart);
+
+//       //Chart Styling
+//       chart.gridAboveGraphs = true;
+//       chart.startDuration = 1;
+//       chart.categoryAxis.gridAlpha = 0;
+//       chart.categoryAxis.title = "Percentile";
+
+//       //Add Chart cursor
+//       var chartCursor = new AmCharts.ChartCursor();
+//       chartCursor.zoomable = false;
+//       chartCursor.alpha = 0.9;
+//       chartCursor.cursorColor = '#28384B'
+//       chartCursor.categoryBalloonEnabled = false;
+//       chart.addChartCursor(chartCursor);
+
+//       //Write
+//       chart.write("chart" + index)
+
+
+
+//       function generateData(studentPoints){
+//         console.log("called")
+//         var dataProvider = [];
+//         for (var i=0; i < studentPoints.length; i++){
+//         dataProvider.push({x:((i + 1) * 10) + "th", y:studentPoints[i] })
+//         }
+//         console.log(dataProvider)
+//         chart.dataProvider = dataProvider;
+//         chart.validateNow();
+//       };
+//     });
+//   };
+// })
