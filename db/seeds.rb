ca0ce912557edf46ca20deeb3f0db3c4d66b23ae
brandoshmando <brancraft@gmail.com>
2014-08-22 14:36:58 -0400

    Through Evals, Categories, Comments, Students, and Rosters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 puts "Database Creation Initialized"
 puts
-puts "---Destroying all previous records"
+puts "---Destroying all previous records---"
 Professor.destroy_all
 Assistant.destroy_all
 Course.destroy_all
@@ -17,12 +17,79 @@ puts
 puts "Creating Professor Brando..."
 Professor.create(first_name:"Brandon", last_name:"Craft", email:"brancraft@gmail.com", password:"blah")
 puts
-puts "Creating first course..."
-names = [ "Calculus 1060", "Marketing 1010", "Advertising 4360", "Finance with DogeCoin", "Accounting 3500", "Emerging Markets" ]
+puts "Creating courses..."
+names = [ "Calculus 1060", "Marketing 1010", "Advertising 4360", "Finance with DogeCoin", "Accounting 3500", "Emerging Markets", "Business Ethics 1240" ]
 6.times do |i|
   Course.create(name: names[i], description:Faker::Lorem.paragraph(1), professor_id: 1)
 end
-# puts "Creating Professors..."
+puts
+puts "Creating rosters..."
+@days = %w[Mon Tues Wed Thurs Fri]
+6.times do |i|
+  ros = Roster.create
+  ros.section_name = "First Section"
+  ros.meet_time = rand(24)
+  ros.meet_day = @days[rand(4)]
+  ros.location = ["PLD 124", "OSH 206", "BUT 405", "HPER 125"][rand(3)]
+  ros.course_id = i
+  ros.save
+end
+puts
+puts "Creating Students..."
+puts
+@counter = 1
+140.times do |i|
+  if i % 20 == 0 && i != 0
+    @counter += 1
+    puts "---#{i} students created---"
+    puts
+  end
+  stud = Student.create
+  name = Faker::Name.name.split(' ')
+
+  stud.first_name = name[0]
+  stud.last_name = name[1]
+  stud.email = Faker::Internet.safe_email(name[0])
+  stud.uid = Faker::Number.number(6)
+  stud.roster_id = @counter
+  stud.save
+end
+puts "---140 students created---"
+puts
+puts "Creating categories..."
+@counter = 1
+36.times do |i|
+  if i % 2 == 0 && i !=0
+    @counter += 1
+  end
+  cat = Category.create
+  cat.title = Faker::Commerce.product_name
+  cat.max_score = 50.to_s
+  cat.eval_id = @counter
+  cat.save
+end
+puts
+puts "Creating Comments..."
+@counter = 1
+62.times do |i|
+  if i % 2 == 0 && i != 0
+    @counter += 1
+  end
+  Comment.create(description:Faker::Lorem.paragraph(1), category_id: @counter)
+end
+puts
+puts "Creating Evals..."
+@counter = 1
+18.times do |i|
+  if i % 3 == 0 && i != 0
+    @counter += 1
+  end
+  eval = Eval.create(title: "Eval #{[i + 1]}" , professor_id: 1)
+  eval.courses << Course.find(@counter)
+  eval.save
+end
+
+# psuts "Creating Professors..."
 # 10.times do |i|
 #   name = Faker::Name.name.split(' ')
 #   prof = Professor.create(first_name: name[0], last_name: name[1], email: Faker::Internet.safe_email(name[0]), crypted_password:Faker::Internet.password(8))
