4a57d1f53e2e38a001ca2657666ef47e5fc69ab3
brandoshmando <brancraft@gmail.com>
2014-08-22 13:17:29 -0400

    Fixed config file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: ,
-    password: ,
+    user_name: '',
+    password: '',
     authentication: 'plain',
     enable_starttls_auto: true
   }
