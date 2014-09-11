a7fa6929a4123b2390e148732b0f8c702a1d6ca4
brandoshmando <brancraft@gmail.com>
2014-09-11 19:04:03 -0400

    Changed class names to dial because that all I need. Apparently I was way overcomplicating this process. Also added the div back for progress.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,8 @@
 $(document).on('page:load ready', function(){
-  var avgs = $('.avg-dial')
+  var avgs = $('.dial')
 
   for (var i=0; i<avgs.length; i++){
-    var max = JSON.parse(avgs[i].dataset.max)
     $(avgs[i]).knob({
-      'max': max,
       'readOnly': true
     })
   }
