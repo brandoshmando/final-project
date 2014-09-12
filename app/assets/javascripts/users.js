04db22edeefde29bc24fbf53a6de82d259a6d3cc
brandoshmando <brancraft@gmail.com>
2014-09-11 21:18:17 -0400

    Progress rings MOSTLY working. NaN for zero's and the input not displaying in the proper place.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,14 @@
 $(document).on('page:load ready', function(){
   var avgs = $('.dial')
-
+  var first = $('.info-card-knob')[0]
   for (var i=0; i<avgs.length; i++){
     $(avgs[i]).knob({
-      'readOnly': true
+      'readOnly': true,
+      'lineCap': 'round',
+      'thickness': 0.3,
+      'width': parseFloat($(first).css('width')) - 15,
+      'fgColor': '#2DD959',
+      'font': 'inherit'
     })
   }
 })
