0c0352907a8f4993fdc61b17f04f61b190d838f4
brandoshmando <brancraft@gmail.com>
2014-09-12 13:28:34 -0400

    Added proper formatting and got rid of NaN bug

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,9 +9,24 @@ $(document).on('page:load ready', function(){
       'width': parseFloat($(first).css('width')) - 15,
       'height': parseFloat($(first).css('width')) - 15,
       'fgColor': '#2DD959',
-      'font': 'inherit'
+      'font': 'inherit',
+      'format': function(value){
+        if (isNaN(value)){
+          return 0 + inputFormat(avgs[i])
+        }else{
+          return value + inputFormat(avgs[i])
+        }
+      }
     })
+
+    function inputFormat(input){
+      if(input.dataset.max){
+        return ' pts'
+      }else{
+        return '%'
+      }
+    }
   }
-  // Overrides existging margins to make them RIGHT (spent too much time on that)
+  // Overrides existing margins to make them RIGHT (spent too much time on that)
   $('.dial').css({'margin-top':'-5em', 'margin-left':'1.563em'})
 })
