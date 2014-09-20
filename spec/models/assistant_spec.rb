51183d35d8ed13bf1a5d086f2c5fd0908e2b132a
brandoshmando <brancraft@gmail.com>
2014-09-19 21:20:34 -0400

    Added a few respond_to's

--- spec/models/assistant_spec.rb
+++ spec/models/assistant_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Assistant, :type => :model do
   it {should respond_to(:first_name)}
+  it {should respond_to(:last_name)}
+  it {should respond_to(:email)}
+  it {should respond_to(:type)}
 end
