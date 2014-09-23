be7b8e3509b6af0addb61f4a41f0efd356840ff3
brandoshmando <brancraft@gmail.com>
2014-09-23 16:11:49 -0500

    Tests finally coming along after wrestling with spork and guard for much too long

--- spec/models/assistant_spec.rb
+++ spec/models/assistant_spec.rb
@@ -1,9 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe Assistant, :type => :model do
+describe Assistant, :type => :model do
   it {should respond_to(:first_name)}
   it {should respond_to(:last_name)}
   it {should respond_to(:email)}
   it {should respond_to(:type)}
+  it {should respond_to(:crypted_password)}
+  it {should respond_to(:salt)}
   it {should respond_to(:activation_state)}
+  it {should respond_to(:activation_token)}
+  it {should respond_to(:activation_token)}
+  it {should respond_to(:activation_token_expires_at)}
+  it {should respond_to(:reset_password_token)}
+  it {should respond_to(:reset_password_token_expires_at)}
+  it {should respond_to(:reset_password_email_sent_at)}
+  it {should respond_to(:remember_me_token)}
+  it {should respond_to(:remember_me_token_expires_at)}
+  it {should respond_to(:avatar_file_name)}
+  it {should respond_to(:avatar_content_type)}
+  it {should respond_to(:avatar_file_size)}
+  it {should respond_to(:avatar_updated_at)}
+
+  before(:each) do
+    @assistant = Assistant.new(first_name: "Brandon", last_name: "Shmando", email: "shmando@example.com", password: "Password")
+  end
+
+  describe "#new" do
+    it "should return a new instance of Assistant" do
+      expect(@assistant).to be_a Assistant
+    end
+
+    it "should have #type 'Assistant'" do
+      expect(@assistant.type).to eq("Assistant")
+    end
+  end
 end
