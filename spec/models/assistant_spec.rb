ce98d47ccd1d6e997de11f41c726dda1d7c6d17c
brandoshmando <brancraft@gmail.com>
2014-09-23 11:26:53 -0500

    activation state test

--- spec/models/assistant_spec.rb
+++ spec/models/assistant_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Assistant, :type => :model do
   it {should respond_to(:last_name)}
   it {should respond_to(:email)}
   it {should respond_to(:type)}
+  it {should respond_to(:activation_state)}
 end
