1b986b1680aba8a0afa1d25957e15cdc2a420857
brandoshmando <brancraft@gmail.com>
2014-09-25 15:40:46 -0500

    Factories for the two different types of users

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+Factorygirl.define do
+  factory :assistant do |f|
+    f.first_name "Brando"
+    f.last_name "Shmando"
+    f.email "shmando@example.com"
+  end
+
+  factory :professor do |f|
+    f.first_name "Brando"
+    f.last_name "Shmando"
+    f.email "profshmando@example.com"
+  end
+end
