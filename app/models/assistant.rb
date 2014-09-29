bc598aaefdc23b346e2c7feb1b4896379794fc0a
brandoshmando <brancraft@gmail.com>
2014-09-29 12:26:17 -0500

    Starting validations

--- app/models/assistant.rb
+++ app/models/assistant.rb
@@ -1,5 +1,6 @@
 class Assistant < User
   has_and_belongs_to_many :courses, join_table: 'assistants_courses'
+  validates_associated :courses
 
   #Redefines model name method to return User.model_name instead. This lets each class use its parents path helpers
   def self.model_name
