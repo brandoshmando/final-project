fb1a8a4f49c5b7893204dd22f7098a6547c60860
brandoshmando <brancraft@gmail.com>
2014-11-06 18:21:46 -0500

    Testing using text editor to do proper git commits...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  #Test comment for git. Delete if you see this...
   protect_from_forgery with: :exception
   before_filter :require_login
 
